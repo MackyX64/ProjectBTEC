2288e9df5b288eaa22bc3a0be82c8affb119786c
MackyX64 <macfitzwool99@gmail.com>
2019-01-03 16:30:34 +0000

    Project Imported
    Uploaded base file to begin from for starting the project.
    Base template with no specific code applied.

---
+++ project_bikelite/prototype/BikeIndCtrl_ProtoMk1/BikeIndCtrl_ProtoMk1.ino
@@ -0,0 +1,16 @@
+// Bicycle Indicator System v0.1
+// Version: PROTO_1.0
+// Desc: This is the main program that controls the light system and activates the corresponding light via a wireless signal.
+// Requirements for Board:
+// > 433 MHz Receiver Module
+// > RCSwitch Library
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
